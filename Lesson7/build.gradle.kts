21175a33e4412c40068a8f9fe88fc8ca6fa94a14
d0pus <mopsuspro@gmail.com>
2025-12-23 17:47:41 +0300

    ///

--- Lesson7/build.gradle.kts
+++ Lesson7/build.gradle.kts
@@ -1,43 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-}
-
-android {
-    namespace = "ru.mirea.shlobin.lesson7"
-    compileSdk = 36
-
-    defaultConfig {
-        applicationId = "ru.mirea.shlobin.lesson7"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.gms) apply false
 }
